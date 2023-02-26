039101bcca1b279500a6e5048f102446a4223dc7
aleksandrlobanov <kitalex777@mail.ru>
2023-02-26 23:16:17 +0300

    Initial commit

---
+++ src/main/java/skay/pro/course4/course4/Course4Application.java
@@ -0,0 +1,13 @@
+package skay.pro.course4.course4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course4Application.class, args);
+	}
+
+}
---
+++ src/test/java/skay/pro/course4/course4/Course4ApplicationTests.java
@@ -0,0 +1,13 @@
+package skay.pro.course4.course4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
